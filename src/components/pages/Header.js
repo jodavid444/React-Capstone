316a2246b181d9440c480a58ee77d58c40f22f52
Joseph david <jodavid444dave@gmail.com>
2023-03-16 08:48:40 +0000

    Modify style.css file, ammend styling

--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from '../../images/123123.png';
+import logo from '../../images/613625ccf5966900044cbf6e.png';
 
 function Header() {
   return (
     <header>
       <img src={logo} alt="logo" className="logo" />
-      <h1>Grub Hub Foods Tract </h1>
+      <h1>The Grub Hub Republic</h1>
       <i className="fa-solid fa-microphone" />
     </header>
   );
